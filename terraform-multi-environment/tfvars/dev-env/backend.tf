508b05bfec66285484f7a6bac76f1b58eabdcedf
prem DevOps Engineer <premdevops@gmail.com>
2026-03-14 21:28:05 +0530

    tfvars

---
+++ terraform-multi-environment/tfvars/dev-env/backend.tf
@@ -0,0 +1,5 @@
+bucket       = "prem-remote-state-tfvars" # bucket name 
+key          = "remote-state.tfstate-dev"
+region       = "us-east-1"
+encrypt      = true
+use_lockfile = true
---
+++ terraform-multi-environment/tfvars/prod/backend.tf
@@ -0,0 +1,5 @@
+bucket       = "prem-remote-state-tfvars" # bucket name 
+key          = "remote-state.tfstate-prod"
+region       = "us-east-1"
+encrypt      = true
+use_lockfile = true
